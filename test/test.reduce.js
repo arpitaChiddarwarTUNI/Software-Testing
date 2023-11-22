d2996dbe26d040115cde73a0008cec8948fa9c83
Johanna Jaatinen <johanna.jaatinen@tuni.fi>
2023-11-22 17:52:10 +0200

    test.reduce.js syntax change

--- test/test.reduce.js
+++ test/test.reduce.js
@@ -2,56 +2,62 @@ import { expect } from 'chai';
 import reduce from '../src/reduce.js';
 
 // Test Case ID TC-RU-22
-test('reduce should correctly reduce an array of numbers', () => {
-  // Test Setup
-  const numbers = [1, 2, 3, 4];
+describe('reduce', function() {
+  it('should correctly reduce an array of numbers', function() {
+    // Test Setup
+    const numbers = [1, 2, 3, 4];
 
-  // Test Execution
-  const result = reduce(numbers, (accumulator, value) => accumulator + value, 0);
+    // Test Execution
+    const result = reduce(numbers, (accumulator, value) => accumulator + value, 0);
 
-  // Expected Result
-  expect(result).to.equal(10);
-});
-
-// Test Case ID TC-RU-23
-test('reduce should create a dictionary by category for an array of food products', () => {
-  // Test Setup
-  const products = [
-    { name: 'Apple', category: 'Fruits' },
-    { name: 'Banana', category: 'Fruits' },
-    { name: 'Carrot', category: 'Vegetables' },
-    { name: 'Bread', category: 'Bakery' },
-  ];
-
-  // Test Execution
-  const result = reduce(
-    products,
-    (result, product) => {
-      if (!result[product.category]) {
-        result[product.category] = [];
-      }
-      result[product.category].push(product.name);
-      return result;
-    },
-    {}
-  );
-
-  // Expected Result
-  expect(result).to.deep.equal({
-    Fruits: ['Apple', 'Banana'],
-    Vegetables: ['Carrot'],
-    Bakery: ['Bread'],
+    // Expected Result
+    expect(result).to.equal(10);
   });
 });
 
 // Test Case ID TC-RU-23
-test('reduce should handle non-array input by returning an empty array', () => {
-  // Test Setup
-  const array = [];
+describe('reduce', function() {
+  it('should create a dictionary by category for an array of food products', function() {
+    // Test Setup
+    const products = [
+      { name: 'Apple', category: 'Fruits' },
+      { name: 'Banana', category: 'Fruits' },
+      { name: 'Carrot', category: 'Vegetables' },
+      { name: 'Bread', category: 'Bakery' },
+    ];
 
-  // Test Execution
-  const result = reduce(array, () => true);
+    // Test Execution
+    const result = reduce(
+      products,
+      (result, product) => {
+        if (!result[product.category]) {
+          result[product.category] = [];
+        }
+        result[product.category].push(product.name);
+        return result;
+      },
+      {}
+    );
 
-  // Expected Result
-  expect(result).to.deep.equal([]);
+    // Expected Result
+    expect(result).to.deep.equal({
+      Fruits: ['Apple', 'Banana'],
+      Vegetables: ['Carrot'],
+      Bakery: ['Bread'],
+    });
+  });
+});
+
+// Test Case ID TC-RU-23
+describe('reduce', function() {
+  it('should handle non-array input by returning an empty array', function() {
+    // Test Setup
+    const array = [];
+
+    // Test Execution
+    const result = reduce(array, () => true);
+
+    // Expected Result
+    expect(result).to.deep.equal([]);
+  });
 });
