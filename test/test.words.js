8096a4cbfe7f9b26b0aaf437c9671a18b5d19e22
Johanna Jaatinen <johanna.jaatinen@tuni.fi>
2023-11-23 08:53:39 +0200

    combined test.words into one describe

--- test/test.words.js
+++ test/test.words.js
@@ -1,18 +1,16 @@
 import { expect } from 'chai';
-import words from '../src//words.js';
+import words from '../src/words.js';
 
-// Test Case ID TC-RU-26
-describe('words - Comma-Separated String', () => {
+describe('words', () => {
+  // Test Case ID TC-RU-26
   it('Should convert a comma-separated string into an array of words', () => {
     const input = 'apple, banana, orange';
     const result = words(input);
     const expected = ['apple', 'banana', 'orange'];
     expect(result).to.deep.equal(expected);
   });
-});
 
-// Test Case ID TC-RU-27
-describe('words - Custom Delimiters', () => {
+  // Test Case ID TC-RU-27
   it('Should handle a string with input custom delimiters', () => {
     const input = 'apple/banana&orange';
     const delimiters = /[\/&]/;
@@ -20,10 +18,8 @@ describe('words - Custom Delimiters', () => {
     const expected = ['apple', 'banana', 'orange'];
     expect(result).to.deep.equal(expected);
   });
-});
 
-// Test Case ID TC-RU-28
-describe('words - Unicode Characters', () => {
+  // Test Case ID TC-RU-28
   it('Should handle a string with Unicode characters', () => {
     const input = 'café crème';
     const result = words(input);
