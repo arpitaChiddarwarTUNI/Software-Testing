e13d288896e439b3233c4e4baff7f8e598f1ea6c
Johanna Jaatinen <johanna.jaatinen@tuni.fi>
2023-11-23 15:42:12 +0200

    added more tests for at.js

--- test/test.at.js
+++ test/test.at.js
@@ -1,18 +1,6 @@
 import chai from 'chai';
 import at from '../src/at.js';
 
-/**
- * Happy cases include:
- *  - Desired path is in object
- *  
- * Most relevant exception cases:
- *  - trying to find something that's not in array
- * 
- * Most useful/common error cases:
- * 
- * Other considerations:
- */
-
 describe('Test at.js', function() {
   
   describe('Happy cases: Desired path is in object', function() {
@@ -24,6 +12,31 @@ describe('Test at.js', function() {
 
       chai.expect(result).to.eql(expectedResult);
     });
+
+    // Additional Test Case ID: TC-RU-29
+    it('Happy case 2: should return values from nested object based on provided paths', () => {
+      const items = {
+        fruits: [{ name: 'Apple' }, { name: 'Banana' }],
+        bakery: [{ name: 'Bread' }, { name: 'Cake' }],
+      };
+      const result = at(items, ['fruits[0].name', 'bakery[1].name']);
+      const expectedResult = ['Apple', 'Cake'];
+
+      chai.expect(result).to.deep.equal(expectedResult);
+    });
+
+    // Additional Test Case ID: TC-RU-30
+    it('Happy case 3: should return a value from nested object based on a single path', () => {
+      const items = {
+        fruits: [{ name: 'Apple' }, { name: 'Banana' }],
+        bakery: [{ name: 'Bread' }, { name: 'Cake' }],
+      };
+      const result = at(items, 'fruits[1].name');
+      const expectedResult = ['Banana'];
+
+      chai.expect(result).to.deep.equal(expectedResult);
+    });
+
   });
 
   describe('Most common error cases', function() {
@@ -33,7 +46,4 @@ describe('Test at.js', function() {
         const result = at(object, ['a[0].b.c', 'a[4]']);
         const expectedResult = [3, undefined];
 
-        chai.expect(result).to.eql(expectedResult);
-    });
-  });
-});
+        chai.expect(resu
